ab7195090de184767efb67c48895fb1d14573090
Hamdi Adan <hhhaji013@gmail.com>
2024-02-07 13:18:57 +0300

    new  commit

---
+++ todos-react/src/data/data.js
@@ -0,0 +1,8 @@
+const todos = [
+    { id: 1, title: 'Have lunch'},
+    { id: 2, title: 'Write proposal'},
+    { id: 3, title: 'Journal'},
+    { id: 4, title: 'Finish assignments'},
+]
+
+export default todos;
